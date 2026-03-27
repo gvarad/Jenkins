0f5f69a01a611c63b5e9ce29f13bb865608c74d7
Varad Gaikwad <91328424+gvarad@users.noreply.github.com>
2026-03-27 17:50:04 +0530

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,1 +1,29 @@
-
+pipeline {
+    agent any
+    stages {
+        stage('pull') {
+            steps {
+                echo 'this is pull stage'
+                git 'https://github.com/AnupDudhe/studentapp-ui.git'
+            }
+        }
+        stage('build') {
+            steps {
+                echo 'this is pull stage'
+                sh 'echo we are in build stage'
+            }
+        }
+        stage('test') {
+            steps {
+                echo 'this is pull stage'
+                sh 'echo we are in test stage'
+            }
+        }
+            stage('deploy') {
+            steps {
+                echo 'this is pull stage'
+                sh 'echo we are in deploy stage'
+            }
+        }
+    }
+}
